600770dcd5322507c254531fe6f437780b2b4cfb
Alpha2005elhagary <alphaelhagary@gmail.com>
2025-09-15 22:44:52 +0300

    onboarding and splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-
+import 'package:flutter/material.dart';
 import 'package:linkedin/features/auth/presentation/screens/OnboardingScreen1.dart';
 
 import 'features/auth/presentation/screens/OnboardingScreen2.dart';
 import 'features/auth/presentation/screens/OnboardingScreen3.dart';
-import 'package:linkedin/features/auth/presentation/screens/set_new_password_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       home: OnboardingScreen1(),
       routes: {
